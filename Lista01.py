56b8f0dcb0ac222e7f10c2bc6c8dcd915805fa4f
PedroLPMedeiros <medeiros.pereira1@escolar.ifrn.edu.br>
2024-07-13 15:17:08 -0300

    Lista01.py

--- Lista01.py
+++ Lista01.py
@@ -1,1 +1,25 @@
+#Questão 1
 
+# val_1 = int(input("Insira um valor: "))
+# val_2 = int(input("Insira outro valor: "))
+# soma = val_1 + val_2
+# multiplicacao = val_1 * val_2
+# subtracao = val_1 - val_2
+# divisao = val_1 / val_2
+# print ("A soma desses números é: ", soma)
+# print ("A subtração desses números é: ", subtracao)
+# print ("A multiplicação desses números é: ", multiplicacao)
+# print ("A divisão desses números é: ", divisao)
+# print ("Fim.")
+
+# #Questão 2
+# val_celsius = int(input("Insira um valor de temperatura em Celsius: "))
+# conversao = (val_celsius * 9/5) + 32
+# print ("A temperatura", val_celsius,"ºC em Farenheint é de: ", conversao, "ºF")
+
+# #Questão 3
+# import math
+# pi = math.pi
+# raio = int(input("Insira o valor do raio de um cículo: "))
+# area_circulo = pi * (raio**2)
+# print ("A área do cículo é: ", area_circulo)
